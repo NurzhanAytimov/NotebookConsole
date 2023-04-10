70a7c9a105f1b2eb5812afa31ec63667c8669f45
= <nurzhan.aytimov@gmail.com>
2023-04-10 19:25:38 +0600

    NotebookConsole
    Console version of notepad

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using Notebook;
+using System;
+
+namespace NoteBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextDoc textDoc = new TextDoc();
+            Console.WriteLine("Select category: ");
+            Console.WriteLine("[1] Write text");
+            Console.WriteLine("[2] Reader text");
+            Console.WriteLine("[3] Search text ");
+            Console.WriteLine("[4] Delete text");
+            Console.WriteLine("[5] Exit");
+            int res = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (res == 1)
+                {
+                    Console.WriteLine("Write text:  ");
+                    Console.WriteLine("-------------------");
+                    string text = Console.ReadLine();
+                    textDoc.FileWrite(text);
+                    break;
+                }
+
+                else if (res == 2)
+                {
+                    Console.WriteLine("Read text: ");
+                    Console.WriteLine("----------------");
+                    textDoc.FileRead();
+                    break;
+                }
+                else if (res == 3)
+                {
+                    Console.WriteLine("Enter the text you want to find: ");
+                    Console.WriteLine("------------------");
+                    string text = Console.ReadLine();
+                    textDoc.FileSearch(text);
+
+                }
+                else if (res == 4)
+                {
+                    Console.WriteLine("[1] Delete specific text");
+                    Console.WriteLine("[2] Clear file");
+                    int number = int.Parse(Console.ReadLine());
+
+                    if(number == 1)
+                    {
+                        Console.WriteLine("Enter the text you want to delete: ");
+                        Console.WriteLine("-----------------");
+                        string delete = Console.ReadLine();
+                        textDoc.FileDeleteText(delete);
+                    
+                        break;
+                    }
+                    else
+                    {
+                        textDoc.FileClear();
+                        break;
+                    }
+
+                }
+
+                else
+                {
+                    break;
+                }
+
+            } while (res != 5);
+
+           
+        }
+    }
+}
---
+++ TextDoc.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    
+    public class TextDoc
+    {
+        //Имя файла
+       public string fileName = @"C:\\Users\\PC\\source\repos\\Notebook\\notebook.txt";
+
+     //Метод для записи в блокнот
+       public void FileWrite(string text)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(fileName, FileMode.Append);
+
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+                streamWriter.Write(text);
+
+                streamWriter.Close();
+                fileStream.Close();
+
+                Console.WriteLine("Text written!!");
+            }catch(Exception ex) 
+            {
+                Console.WriteLine("Error!");
+            }
+            
+        }
+        //Метод для вывода в консоль текста в блокноте
+        public void FileRead()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(fileName);
+                string text = streamReader.ReadToEnd(); 
+                Console.WriteLine(text);
+                streamReader.Close();
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Error!");
+            };
+
+        }
+        //метод для пойска 
+        public void FileSearch(string text)
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(fileName);
+
+                string texts = streamReader.ReadToEnd();
+                string[] words = texts.Split('.');
+
+                foreach (string word in words) 
+                {
+                    if (word.Contains(text))
+                    {
+                        Console.WriteLine("Find text: " + word);
+                    }
+                   
+                }
+
+                streamReader.Close();   
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Not find!");
+
+            }
+        }
+
+        //метод для удаления текста
+        public void FileDeleteText(string text)
+        {
+            string fileText;
+            try
+            {
+                using(StreamReader stream = new StreamReader(fileName))
+                {
+                    fileText = stream.ReadToEnd();
+                }
+                fileText = fileText.Replace(text, "");
+
+                using(StreamWriter streamWriter = new StreamWriter(fileName))
+                {
+                    streamWriter.Write(fileText);
+                }
+
+                Console.WriteLine("Text delete!");
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+        //метод для очистки файла полностью
+        public void FileClear()
+        {
+            try
+            {
+                StreamWriter stream = new StreamWriter(fileName);
+                stream.Write("");
+
+                Console.WriteLine("File clear!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+
+
+       
+    }
+
+    
+}
